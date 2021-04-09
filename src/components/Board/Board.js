000d5c0e0b2798187e500cbc153f7143413a4bd3
Vibha Malviya <vibha.malviya@gmail.com>
2021-04-08 18:08:41 -0700

    move state to Game, add time travel functionality

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,37 +1,14 @@
-import React, { useState } from 'react';
-import calculateWinner from '../../utils/CalculateWinner';
 import Square from "../Square/Square";
 import "./Board.css";
 
-function Board() {
-	const [squares, setSquare] = useState(Array(9).fill(null));
-	const [isXNext, setNext] = useState(true);
-	const onSquareClick = (i) => {
-		if(calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		const squares1 = squares.slice();
-		squares1[i] = isXNext ? 'X' : '0';
-		setSquare(squares1);
-		setNext(!isXNext);
-	};
-	
-	const resetBoard = () => {
-		setSquare(Array(9).fill(null));
-		setNext(true);
-	};
-	
-	return <div>
-		{'Winner is ' + calculateWinner(squares)}
-		<div className="board">
+function Board({ squares, onSquareClick}) {
+	return <div className="board">
 			{squares.map((square, index) => {
 				return <Square key={index}
 				               value={square}
 				               clickHandler={() => onSquareClick(index)}/>
 			})}
-		</div>
-		<button onClick={resetBoard}>Restart Game</button>
-	</div>
+		</div>;
 }
 
 export default Board;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,39 @@
+import React, { useState } from 'react';
+
 import Board from "../Board/Board";
 import "./Game.css";
+import calculateWinner from "../../utils/CalculateWinner";
 
 function Game() {
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [isXNext, setNext] = useState(true);
+	const [step, setStep] = useState(0);
+	
+	const onSquareClick = (i) => {
+		const squares = history.slice(0, step + 1);
+		const current = [...squares[step]];
+		if(calculateWinner(current) || current[i]) {
+			return;
+		}
+		current[i] = isXNext ? 'X' : 'O';
+		setHistory([...squares, current]);
+		setStep(squares.length);
+		setNext(!isXNext);
+	};
+	
+	const stepClickHandler = (step) => {
+		setStep(step);
+		setNext(step%2 === 0);
+	};
+	
 	return <div className="game">
-		<Board/>
+		<h1>{'Winner is ' + calculateWinner(history[step])}</h1>
+		<Board squares={history[step]} onSquareClick={onSquareClick}/>
+		<ul>
+			{history.map((squares, step) => {
+				return <li><button key={step} onClick={() => stepClickHandler(step)}>{step ? `Go to step #${step}` : 'Restart game'}</button></li>;
+			})}
+		</ul>
 	</div>;
 }
 
